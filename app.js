cc3bea764016d661be58c594443f3652b400c05b
Chiang <tien23@gmail.com>
2023-04-07 10:51:50 +0800

    feat:add user modules

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -15,7 +14,7 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(routes)
