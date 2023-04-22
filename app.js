3a0fbf8a219975478f4e2f8cbc487002cce7f3ba
Chiang <tien23@gmail.com>
2023-04-23 05:09:49 +0800

    feat:add express-session

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const methodOverride = require('method-override')
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -16,6 +16,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 
