8105b1bd2f591478cfe07d1769cb969036a10060
Chiang <tien23@gmail.com>
2023-04-24 06:38:07 +0800

    feat:add user authentication

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/users',users)
-router.use('/todos', todos)
-router.use('/', home)
+router.use('/users', users)
+router.use('/todos', authenticator, todos)
+router.use('/', authenticator, home)
 
 module.exports = router
